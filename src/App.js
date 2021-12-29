93513087c4186444a824c6fa0e82bc61c1102fc9
jclamy <jctlamy@gmail.com>
2021-12-29 17:27:52 +0100

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+//Update Switch doesnt exist anymore, needs to be replaced by Routes, and Route should look like <Route path='/login' component={<Nav/>}
 
 function App() {
-  return <div className='App'>Amazon clone</div>;
+  return (
+    <Router>
+      <div className='app'>
+        <Routes>
+          <Route path='/checkout'>
+            <h1>Checkout</h1>
+          </Route>
+          <Route path='/login'>
+            <h1>Login page</h1>
+          </Route>
+          {/*Bottom route is always the default route*/}
+          <Route path='/'>
+            <h1>Home page</h1>
+          </Route>
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
