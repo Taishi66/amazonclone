703c1a642a96844ad2b05ebd42ff8c72857b8ac8
jclamy <jctlamy@gmail.com>
2021-12-30 16:11:53 +0100

    add ui-material lib && start navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from "react";
+import * as React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 //Update Switch doesnt exist anymore, needs to be replaced by Routes, and Route should look like <Route path='/login' component={<Nav/>}
+import Header from "./components/Header";
 
 function App() {
   return (
-    <Router>
-      <div className='app'>
-        <Routes>
-          <Route path='/checkout'>
-            <h1>Checkout</h1>
-          </Route>
-          <Route path='/login'>
-            <h1>Login page</h1>
-          </Route>
-          {/*Bottom route is always the default route*/}
-          <Route path='/'>
-            <h1>Home page</h1>
-          </Route>
-        </Routes>
-      </div>
-    </Router>
+    <div className='app'>
+      <Routes>
+        <Route path='/checkout' />
+        <Route path='/login' />
+        {/*Bottom route is always the default route*/}
+        <Route path='/' element={<Header />} />
+      </Routes>
+    </div>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,44 @@
 import React from "react";
+import "../Header.css";
+import { Link } from "react-router-dom";
+import SearchIcon from "@mui/icons-material/Search";
 
 function Header() {
-  return <div></div>;
+  return (
+    <div className='header'>
+      <Link to='/'>
+        <img
+          className='header__logo'
+          src='http://pngimg.com/uploads/amazon/amazon_PNG11.png'
+          alt='Amazon logo'
+        />
+      </Link>
+      <div className='header__search'>
+        <input type='text' className='header__searchInput' />
+        <SearchIcon className='header__searchIcon' />
+      </div>
+      <div className='header__nav'>
+        <Link to='/login' className='header__link'>
+          <div className='header__option'>
+            <span>Hello</span>
+            <span>Sign in</span>
+          </div>
+        </Link>
+        <Link to='/' className='header__link'>
+          <div className='header__option'>
+            <span>Returns</span>
+            <span>& orders</span>
+          </div>
+        </Link>
+        <Link to='/' className='header__link'>
+          <div className='header__option'>
+            <span>Your</span>
+            <span>Prime</span>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
