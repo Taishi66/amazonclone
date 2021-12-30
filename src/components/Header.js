b4204d03734f036371152eaa9cd3d7df2c4b17f1
jclamy <jctlamy@gmail.com>
2021-12-30 16:34:10 +0100

    navbar styling done

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import "../Header.css";
 import { Link } from "react-router-dom";
 import SearchIcon from "@mui/icons-material/Search";
+import ShoppingBasket from "@mui/icons-material/ShoppingBasket";
 
 function Header() {
   return (
@@ -13,27 +14,38 @@ function Header() {
           alt='Amazon logo'
         />
       </Link>
+
       <div className='header__search'>
         <input type='text' className='header__searchInput' />
         <SearchIcon className='header__searchIcon' />
       </div>
+
       <div className='header__nav'>
         <Link to='/login' className='header__link'>
           <div className='header__option'>
-            <span>Hello</span>
-            <span>Sign in</span>
+            <span className='header__optionLineOne'>Hello</span>
+            <span className='header__optionLineTwo'>Sign in</span>
           </div>
         </Link>
+
         <Link to='/' className='header__link'>
           <div className='header__option'>
-            <span>Returns</span>
-            <span>& orders</span>
+            <span className='header__optionLineOne'>Returns</span>
+            <span className='header__optionLineTwo'>& orders</span>
           </div>
         </Link>
+
         <Link to='/' className='header__link'>
           <div className='header__option'>
-            <span>Your</span>
-            <span>Prime</span>
+            <span className='header__optionLineOne'>Your</span>
+            <span className='header__optionLineTwo'>Prime</span>
+          </div>
+        </Link>
+
+        <Link to='/checkout' className='header__link'>
+          <div className='header__optionBasket'>
+            <ShoppingBasket />
+            <span className='header__optionLineTwo header__basketCount'>0</span>
           </div>
         </Link>
       </div>
