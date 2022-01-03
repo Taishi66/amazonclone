4eb20b296e40e4ea0bbe5ba1249a732a9ce5d042
jclamy <jctlamy@gmail.com>
2022-01-03 09:46:06 +0100

    add product cards

--- src/components/products/Product.js
+++ src/components/products/Product.js
@@ -1,8 +1,30 @@
 import React from "react";
 import "./Product.css";
+import Star from "@mui/icons-material/Star";
 
-function product() {
-  return <div></div>;
+function product({ id, title, image, price, rating }) {
+  return (
+    <div className='product'>
+      <div className='product__info'>
+        <p>{title}</p>
+        <p className='product__price'>
+          <small>€</small>
+          <strong>{price}</strong>
+        </p>
+        <div className='product__rating'>
+          {Array(rating)
+            .fill()
+            .map((_) => (
+              <p>
+                <Star />
+              </p>
+            ))}
+        </div>
+        <img src={image} alt='' />
+        <button>Add to basket</button>
+      </div>
+    </div>
+  );
 }
 
 export default product;
