5fe74c8b7b2fd23d60278b8720e1975a9f7dc5df
jclamy <jctlamy@gmail.com>
2022-01-03 14:00:55 +0100

    testing ADD_TO_BASKET

--- src/components/StateProvider/reducer.js
+++ src/components/StateProvider/reducer.js
@@ -14,7 +14,7 @@ const reducer = (state, action) => {
         basket: [...state.basket, action.item],
       };
     case "REMOVE_FROM_BASKET":
-      //logic  to remove from basket
+      //logic to remove from basket
       return { state };
     default:
       return state;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,6 +7,7 @@ import { useStateValue } from "../StateProvider/StateProvider";
 
 function Header() {
   const [{ basket }] = useStateValue();
+  console.log(basket);
 
   return (
     <div className='header'>
