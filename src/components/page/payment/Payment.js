6a28bad3365ad733271d0c6ee6099b85759bf9bd
jclamy <jctlamy@gmail.com>
2022-01-06 16:25:05 +0100

    adding && styling payment.js

--- src/components/page/payment/Payment.js
+++ src/components/page/payment/Payment.js
@@ -1,15 +1,50 @@
-import React from 'react'
+import React from "react";
 import "./Payment.css";
-
+import { useStateValue } from "../../StateProvider/StateProvider";
+import CheckoutProduct from "../../checkoutProduct/CheckoutProduct";
+import { Link } from "react-router-dom";
 
 function Payment() {
-    return (
-        <div className="payment">
-            <div className="payment__container">
+  // eslint-disable-next-line no-unused-vars
+  const [{ user, basket }, dispatch] = useStateValue();
 
-            </div>
+  return (
+    <div className='payment'>
+      <div className='payment__container'>
+        <h1>
+          Checkout (<Link to='/checkout'>{basket?.length} items</Link>)
+        </h1>
+        <div className='payment__section'>
+          <div className='payment__title'>
+            <h3>Delivery Address</h3>
+          </div>
+          <div className='payment__address'>
+            <p>{user?.email}</p>
+          </div>
         </div>
-    )
+        <div className='payment__section'>
+          <div className='payment__title'>
+            <h3>Review items & delivery</h3>
+          </div>
+          <div className='payment__items'>
+            {basket.map((item) => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))}
+          </div>
+        </div>
+        <div className='payment__section'>
+          <div className='payment__title'>Payment Method</div>
+          <div className='payment__details'>{/*use Stripe magic*/}</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Payment;
--- src/components/subtotal/SubTotal.js
+++ src/components/subtotal/SubTotal.js
@@ -6,34 +6,37 @@ import { getBasketTotal } from "../StateProvider/reducer";
 import { useNavigate } from "react-router-dom";
 
 function SubTotal() {
+  const navigate = useNavigate(); //react router v6 doesnt use useHistory anymore =>>history('/', { replace: true });
+  const [{ basket }] = useStateValue();
 
-    const history = useNavigate();//react router v6 doesnt use useHistory anymore =>>history('/', { replace: true });
-
-    const [{ basket }] = useStateValue();
-
-    return (
-        <div className="subtotal">
-            {/* Price */}
-            <CurrencyFormat
-                renderText={(value) => (
-                    <React.Fragment>
-                        <p>
-                            Subtotal ({basket?.length} items): <strong>{value}</strong>
-                        </p>
-                        <small className="subtotal__gift">
-                            <input type="checkbox" /> This order contains a gift
-                        </small>
-                    </React.Fragment>
-                )}
-                decimalScale={2}
-                value={getBasketTotal(basket)}
-                displayType={"text"}
-                thousandSeparator={true}
-                prefix={"$"}
-            />
-            <button onClick={history('/payment')}>Proceed to checkout</button>
-        </div >
-    );
+  return (
+    <div className='subtotal'>
+      {/* Price */}
+      <CurrencyFormat
+        renderText={(value) => (
+          <React.Fragment>
+            <p>
+              Subtotal ({basket?.length} items): <strong>{value}</strong>
+            </p>
+            <small className='subtotal__gift'>
+              <input type='checkbox' /> This order contains a gift
+            </small>
+          </React.Fragment>
+        )}
+        decimalScale={2}
+        value={getBasketTotal(basket)}
+        displayType={"text"}
+        thousandSeparator={true}
+        prefix={"$"}
+      />
+      <button
+        onClick={() => {
+          navigate("/payment");
+        }}>
+        Proceed to checkout
+      </button>
+    </div>
+  );
 }
 
 export default SubTotal;
